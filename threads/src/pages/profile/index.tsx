2ed13cdc6bdd349c79bf69f1f3d29c01487ab21a
Willian Hercilio Pezzini <williantijucas@gmail.com>
2025-04-25 07:13:45 -0300

    Finalizado projeto

--- threads/src/pages/profile/index.tsx
+++ threads/src/pages/profile/index.tsx
@@ -1,13 +1,19 @@
 import React, {useState} from "react";
 
-import { Container, Logo, Header, DivNome, DivFotoPerfil, Nome, NomeDePerfil, BotaoThreads, DivNomePerfil, FotoPerfil, Biografia, TextoBiografia, ContainerSeguidoresLink, DivseguidoresLink, DivIconeInstagram, TextoCostomizavel, Icone, ContainerThreadsOuRespostas, DivThreadsOuRespostas, ContainerThreads, DivFotoPerfilContainer, DivPostThreadsContainer, DivHeaderPostContainer, PerfilThreads, TempoDoPost} from "./styled";
+import { Container, Logo, Header, DivNome, DivFotoPerfil, Nome, NomeDePerfil, BotaoThreads, DivNomePerfil, FotoPerfil, Biografia, TextoBiografia, ContainerSeguidoresLink, DivseguidoresLink, DivIconeInstagram, TextoCostomizavel, Icone, ContainerThreadsOuRespostas, DivThreadsOuRespostas, ContainerThreads, DivFotoPerfilContainer, DivPostThreadsContainer, DivHeaderPostContainer, PerfilThreads, TempoDoPost, DivPost, DivInteracaoPost, ContainerDownload, BotaoBaixarApp } from "./styled";
 
 import LogoSVG from "../../assets/icons/logo.svg";
 import FotoDePerfil from "../../assets/images/perfil.jpg";
 import LogoInstagram from "../../assets/icons/instagram-logo.svg";
 import LogoGitHub from "../../assets/icons/github-logo.svg";
+import LogoThreads from "../../assets/icons/Logo-Threads.svg"
 import BotaoMore from "../../assets/icons/more-circle.svg";
 import More from "../../assets/icons/more-svg.svg";
+import Curtir from "../../assets/icons/favorite.svg";
+import Comentar from "../../assets/icons/comment-right.svg";
+import Atualizar from "../../assets/icons/update.svg";
+import Enviar from "../../assets/icons/send.svg";
+
 
 const Profile = () => {
 
@@ -57,7 +63,9 @@ const Profile = () => {
 
             <ContainerSeguidoresLink>
                 <DivseguidoresLink>
-                    <TextoCostomizavel color="#616161" size="14">100 Seguidores . <a href="#">willianpezzini.com</a></TextoCostomizavel>
+                    <TextoCostomizavel color="#616161" size="14">
+                        100 Seguidores . <a href="#">willianpezzini.com</a>
+                    </TextoCostomizavel>
                 </DivseguidoresLink>
                    
                 <DivIconeInstagram>
@@ -92,8 +100,36 @@ const Profile = () => {
                             <Icone width='32' src={More}></Icone>
                         </TempoDoPost>
                     </DivHeaderPostContainer>
+
+                    <DivPost>
+                        <TextoCostomizavel color="#ffffff" size="15" marginTop="10">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio cupiditate temporibus omnis adipisci commodi quae dolore laudantium. Quidem aspernatur, praesentium illum explicabo saepe eum cumque magnam animi debitis cupiditate natus?
+                        </TextoCostomizavel>
+
+                        <TextoCostomizavel color="#ffffff" size="15" marginTop="10">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta suscipit hic distinctio, natus iure fugiat recusandae voluptatibus sint aliquid labore non atque cum expedita architecto voluptatum quia veniam minus odio!
+                        </TextoCostomizavel>
+                    </DivPost>
+                    <DivInteracaoPost>
+                        <Icone src={Curtir} width="20"></Icone>
+                        <Icone src={Comentar} width="20"></Icone>
+                        <Icone src={Atualizar} width="20"></Icone>
+                        <Icone src={Enviar} width="20"></Icone>
+                    </DivInteracaoPost>
+                    <TextoCostomizavel color="#616161" size="14" marginLeft="5">9 curtidas</TextoCostomizavel>
                 </DivPostThreadsContainer>
+
+                
             </ContainerThreads>
+
+            <ContainerDownload>
+                <Icone src={LogoThreads} width="40" marginBottom="-20" marginLeft="-40"></Icone>
+                <FotoPerfil src={FotoDePerfil} maxWidth="40"></FotoPerfil>
+                <TextoCostomizavel color="#5d5d5d" size="15" marginTop="15">
+                    Baixe o App Threads para ver mais de willianpezzini.  
+                </TextoCostomizavel>
+                <BotaoBaixarApp>Baixar o Threads</BotaoBaixarApp>
+            </ContainerDownload>
         </Container>
 
     );
